df4ff8d0fa9d260a02ac2a862e42759b4d922a61
sergey <martin10@bk.ru>
2017-09-28 20:29:00 +0300

    add oracle

---
+++ src/main/java/BookwormOracle.java
@@ -0,0 +1,6 @@
+public class BookwormOracle implements Oracle {
+    @Override
+    public String gefineMeaningOfLife() {
+        return "Encyclopedias are a waste of money - user the Internet";
+    }
+}
---
+++ src/main/java/XmlConfigWithBeanFactory.java
@@ -0,0 +1,13 @@
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.core.io.ClassPathResource;
+
+public class XmlConfigWithBeanFactory {
+    public static void main(String[] args){
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader rdr = new XmlBeanDefinitionReader(factory);
+        rdr.loadBeanDefinitions(new ClassPathResource("META-INF/spring/xml-bean-factory-config.xml"));
+        Oracle oracle = factory.getBean("oracle",Oracle.class);
+        System.out.println(oracle.gefineMeaningOfLife());
+    }
+}
