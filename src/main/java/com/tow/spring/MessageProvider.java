0d5294fa1303f76c53c311ef56ffcae567203bb1
sergey <martin10@bk.ru>
2017-09-30 09:54:38 +0300

    remove old lesson

--- src/main/java/com/tow/spring/MessageProvider.java
+++
@@ -1,6 +0,0 @@
-package com.tow.spring;
-
-public interface MessageProvider {
-    String getMessage();
-    void setMessage(String message);
-}
--- src/main/java/com/tow/spring/MessageRenderer.java
+++
@@ -1,7 +0,0 @@
-package com.tow.spring;
-
-public interface MessageRenderer {
-    void render();
-    void setMessageProvider(MessageProvider provider);
-    MessageProvider getMessageProvider();
-}
