dcbffbc389572074e6625fff76e81cdfaaa728ba
sergey <martin10@bk.ru>
2017-10-03 00:04:16 +0300

    add init()

--- src/main/java/com/tow/spring/xml/SimpleBean.java
+++ src/main/java/com/tow/spring/xml/SimpleBean.java
@@ -1,23 +1,49 @@
 package com.tow.spring.xml;
 
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import static java.lang.System.out;
 
 public class SimpleBean {
+    public static String DEFAULT_NAME = "Luke Skywalker";
+
     private String name;
     private int age;
 
     public static void main(String[] args) {
+        out.println(Integer.MIN_VALUE + " - " + Integer.MAX_VALUE);
+
         GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
         ctx.load("classpath:META-INF/spring/app-context-xml.xml");
         ctx.refresh();
 
-        SimpleBean parent = ctx.getBean("parent", SimpleBean.class);
-        SimpleBean children = ctx.getBean("child", SimpleBean.class);
+        SimpleBean simpleBean1 = getBean("simpleBean1", ctx);
+        SimpleBean simpleBean2 = getBean("simpleBean2", ctx);
+        SimpleBean simpleBean3 = getBean("simpleBean3", ctx);
+    }
 
-        out.println(parent);
-        out.println(children);
+    public static SimpleBean getBean(String beanName, ApplicationContext ctx) {
+        SimpleBean bean = null;
+        try {
+            bean = ctx.getBean(beanName, SimpleBean.class);
+            out.println(bean);
+        } catch (BeanCreationException e) {
+            out.println("An error occured in bean configuration: " + e.getMessage());
+        }
+        return bean;
+    }
+
+    public void init() {
+        out.println("Initializing bean");
+        if (name == null) {
+            out.println("Using default name");
+            name = DEFAULT_NAME;
+        }
+        if (age == Integer.MIN_VALUE || age == Integer.MAX_VALUE) {
+            throw new IllegalArgumentException("set normal value");
+        }
     }
 
     public void setName(String name) {
