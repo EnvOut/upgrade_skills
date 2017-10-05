6206a7c45c821e3440feb1ba1f49215d65dece6f
sergey <martin10@bk.ru>
2017-10-05 22:28:07 +0300

    add @Annotaions

--- src/main/java/com/tow/spring/xml/AppConfig.java
+++ src/main/java/com/tow/spring/xml/AppConfig.java
@@ -1,12 +1,21 @@
 package com.tow.spring.xml;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@ImportResource(value = "classpath:META-INF/spring/app-context-xml.xml")
+@PropertySource("classpath:message.properties")
+@ComponentScan(basePackages = {"com.tow.spring"})
+@EnableTransactionManagement
 public class AppConfig {
+    @Autowired
+    Environment env;
+
     @Bean
+    @Scope(value = "prototype")
     public MessageRenderer messageRenderer(@Autowired MessageProvider messageProvider) {
         MessageRenderer messageRenderer = new StandartOutMessageRenderer();
         messageRenderer.setMessageProvider(messageProvider);
@@ -14,9 +23,10 @@ public class AppConfig {
     }
 
     @Bean
+    @Lazy(value = true)
     public MessageProvider messageProvider() {
         ConfigurableMessageProvider messageProvider = new ConfigurableMessageProvider();
-        messageProvider.setMessage("This is a configurable message");
+        messageProvider.setMessage(env.getProperty("message"));
         return messageProvider;
     }
 }
