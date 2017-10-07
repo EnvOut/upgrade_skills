ebf8ab67866f2be318b4103c51ba89fbc0f900f5
Unknown <drenozua@gmail.com>
2017-10-07 23:48:51 +0300

    add AOP user authentification (before)

---
+++ src/main/java/com/tow/spring/annotation/SecureBean.java
@@ -0,0 +1,9 @@
+package com.tow.spring.annotation;
+
+import static java.lang.System.out;
+
+public class SecureBean {
+    public void writeSecureMessage() {
+        out.println("Every time I learn something new, it pushes some old stuff out of my brain");
+    }
+}
