1f487142ae20ebf2d4bc5f328c4556af50c94c5e
Sergey <drenozua@gmail.com>
2017-11-04 23:56:59 +0200

    add xml response

--- src/main/java/com/tow/spring/configuration/AppConfig.java
+++ src/main/java/com/tow/spring/configuration/AppConfig.java
@@ -1,8 +1,13 @@
 package com.tow.spring.configuration;
 
+import com.tow.spring.model.Contacts;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 
 @Import({
         PostgresConfiguration.class,
@@ -11,4 +16,8 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @ComponentScan(basePackages = "com.tow.spring")
 public class AppConfig {
+    @Bean(name = "jaxContext")
+    public JAXBContext jaxbContext() throws JAXBException {
+        return JAXBContext.newInstance(Contacts.class);
+    }
 }
--- src/main/java/com/tow/spring/controller/CrudController.java
+++ src/main/java/com/tow/spring/controller/CrudController.java
@@ -2,6 +2,7 @@ package com.tow.spring.controller;
 
 import com.google.common.collect.Lists;
 import com.tow.spring.model.Contact;
+import com.tow.spring.model.Contacts;
 import com.tow.spring.repositories.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -10,6 +11,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Nullable;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.StringWriter;
 import java.util.List;
 
 @RestController
@@ -19,16 +24,33 @@ public class CrudController {
     @Qualifier("contactRepository")
     private ContactRepository contactRepository;
 
+    @Autowired
+    @Qualifier("jaxContext")
+    private JAXBContext jaxbContext;
+
     @GetMapping(path = "json",
             produces = MediaType.APPLICATION_JSON_VALUE
-            )
+    )
 //            headers = {HttpHeaders.ACCEPT, MediaType.TEXT_HTML_VALUE, MediaType.TEXT_XML_VALUE})
 //    @ResponseBody
     public List<Contact> findAll() {
         return Lists.newArrayList(contactRepository.findAll());
     }
 
+    @GetMapping(path = "xml",
+            produces = MediaType.APPLICATION_XML_VALUE)
+    @ResponseBody
+    public String getXml() throws JAXBException {
+        Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
 
+        jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+        //Marshal the employees list in console
+        jaxbMarshaller.marshal(new Contacts(Lists.newArrayList(contactRepository.findAll())), System.out);
+        StringWriter result = new StringWriter();
+        jaxbMarshaller.marshal(new Contacts(Lists.newArrayList(contactRepository.findAll())), result);
+        return result.toString();
+    }
 // @GetMapping(path = {"all", "all.json","all.xml"},consumes = {MediaType.},produces={MediaType.APPLICATION_XML_VALUE},headers = "Accept=application/xml")
 
     @GetMapping("count")
@@ -49,10 +71,9 @@ public class CrudController {
 
     @GetMapping("create")
     public String create(
-            @RequestParam("firstName") @Nullable String firstName,
-            @RequestParam("lastName") @Nullable String lastName,
-            Model model
-    ) {
+            @RequestParam(name = "firstName", required = false) String firstName,
+            @RequestParam(name = "lastName", required = false) String lastName,
+            Model model) {
         String results = null;
         if (firstName != null || lastName != null) {
             Contact contact = new Contact(firstName, lastName);
@@ -63,14 +84,11 @@ public class CrudController {
     }
 
     @GetMapping("remove/{id}")
+    @ResponseBody
     public String remove(
-            @PathVariable(name = "id")
-            @Nullable Long id
-    ) {
+            @PathVariable(name = "id", required = false) Long id) {
         String results = "redirect:/all";
-        if (id != null) {
-            contactRepository.delete(id);
-        }
+        contactRepository.delete(id);
         return results;
     }
 
