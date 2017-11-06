54a79e011b7ad6635272786047ac0e4ae9283d9e
Sergey <drenozua@gmail.com>
2017-11-07 00:37:30 +0200

    add js eval

--- src/main/java/com/tow/spring/ListScriptEngines.java
+++ src/main/java/com/tow/spring/ListScriptEngines.java
@@ -1,20 +1,18 @@
 package com.tow.spring;
 
+import javax.script.ScriptEngine;
 import javax.script.ScriptEngineFactory;
 import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 public class ListScriptEngines {
     public static void main(String[] args) {
         ScriptEngineManager mgr = new ScriptEngineManager();
-        for (ScriptEngineFactory factory : mgr.getEngineFactories()) {
-            String engineName = factory.getEngineName();
-            String languageName = factory.getLanguageName();
-            String version = factory.getLanguageVersion();
-
-            System.out.println(new StringBuffer()
-                    .append("engineName: ").append(engineName)
-                    .append("languageName: ").append(languageName)
-                    .append("version: ").append(version));
+        ScriptEngine jsEngine = mgr.getEngineByName("js");
+        try {
+            jsEngine.eval("print('hello javascript in java')");
+        } catch (ScriptException e) {
+            e.printStackTrace();
         }
     }
 }
